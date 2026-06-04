287db9baa0760564d05becce8b346b64f282ab2d
razinmax <razinmax02@gmail.com>
2026-06-04 11:32:44 +0500

    feat: remove hardcoded backend URL in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import {Banner, News, Event, Data, Review} from "./types.ts";
 
-const url = import.meta.env.VITE_BACKEND_URL || "http://193.23.219.232:8080";
+const url = import.meta.env.VITE_BACKEND_URL || "";
 
 async function SendGet<T extends Data>(endpoint: string): Promise<T> {
     try {
